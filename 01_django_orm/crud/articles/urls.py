746dbb57f04716b381ddfae5f8af8ad2ae38b8f6
heejung-choi <choi951008@naver.com>
2020-06-16 10:38:03 +0900

    DJANGO CRUD 공부

--- 01_django_orm/crud/articles/urls.py
+++ 01_django_orm/crud/articles/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
 ]
--- 01_django_orm/crud/articles/views.py
+++ 01_django_orm/crud/articles/views.py
@@ -8,3 +8,27 @@ def index(request):
         'articles': articles,
     }
     return render(request, 'articles/index.html', context)
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    # 1. new에서 보낸 데이터 받기
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    # 2. db에 저장
+    # article = Article()
+    # article.title = title
+    # # 오른쪽이 new에서 받은 데이터
+    # article.content = content
+    # article.save()
+
+    article = Article(title=title, content=content)
+    #데이터가 유효한지 검사
+    #인자로 들어갈 때에는 =에 공백을 넣지 않는다.
+    article.save()
+
+    #Article.objects.create(title=title, content=content)
+
+    return render(request, 'articles/index.html')
