8cf2b5e88325e17f89e9b774b3ee906d32858183
heejung-choi <choi951008@naver.com>
2020-06-24 10:37:03 +0900

    CustomUserCreationForm 공부

--- 05_either/either/accounts/views.py
+++ 05_either/either/accounts/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
         pass
     else:
-        form = UserCreationForm()
+        # form = UserCreationForm()
+        form = CustomUserCreationForm()
     context = {
         'form': form
     }
-    return render(request, 'accounts/signup.html', context)
+    return render(request, 'accounts/form.html', context)
