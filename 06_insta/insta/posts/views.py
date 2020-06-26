a29b5e847532f4fd965d1467f47f5129bb1b278a
heejung-choi <choi951008@naver.com>
2020-06-26 15:54:00 +0900

    좋아요 기능 이벤트핸들러 구현(자바스크립트)

--- 06_insta/insta/posts/views.py
+++ 06_insta/insta/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
@@ -27,6 +28,7 @@ def create(request):
         'form': form
     }
     return render(request, 'posts/form.html', context)
+    # 랜더링을 해서 보내준다. -> 완전한 html 문서를 만들어준다. 사용자에게 리턴
 
 @login_required
 def like(request, pk):
@@ -37,7 +39,25 @@ def like(request, pk):
     if post in user.like_posts.all():
         # 이미 누른경우
         user.like_posts.remove(post)
+        liked = False
     else:
         # 아직 안누른경우
         user.like_posts.add(post)
-    return redirect('posts:index')
+        liked = True
+    # return redirect('posts:index')
+    context = {
+        'msg': '좋아요 기능이 동작 했습니다.',
+        'liked': liked
+    }
+    return JsonResponse(context)
+    # key value , -> json
+    # json을 파이썬 코드로 바꿔 준다.
+    #{data: {…}, status: 200, statusText: "OK", headers: {…}, config: {…}, …}
+    #config: {url: "/posts/5/like/", method: "get", headers: {…}, transformRequest: Array(1), transformResponse: Array(1), …}
+    #data: {msg: "좋아요 기능이 동작 했습니다."}
+    #headers: {content-length: "87", content-type: "application/json", date: "Fri, 26 Jun 2020 06:36:11 GMT", server: "WSGIServer/0.2 CPython/3.7.6", vary: "Cookie", …}
+    #request: XMLHttpRequest {readyState: 4, timeout: 0, withCredentials: false, upload: XMLHttpRequestUpload, onreadystatechange: ƒ, …}
+    #status: 200
+    #statusText: "OK"
+    #__proto__: Object
+    # 클릭시 해당 요청이 나타나게 된다.
